c5e1a227b0e99c68c07e94ac91fe746d278a7ced
bwilkins96 <ben.jr.speed@gmail.com>
2023-06-29 11:58:03 -0400

    mental log model

--- web_app_final/mhtracker/admin.py
+++ web_app_final/mhtracker/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import MentalLog
 
-# Register your models here.
+admin.site.register(MentalLog)
--- web_app_final/mhtracker/models.py
+++ web_app_final/mhtracker/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class MentalLog(models.Model):
+    RATING_CHOICES = [
+        (5, '5 - Best'),
+        (4, '4'),
+        (3, '3 -Middle'),
+        (2, '2'),
+        (1, '1 - Worst')        
+    ]
+
+    mh_rating = models.IntegerField('mental health rating', choices=RATING_CHOICES, default=5)
+    env_rating = models.IntegerField('daily environment rating', choices=RATING_CHOICES, default=5)
+    diet_change = models.BooleanField('diet change?')
+    exercise = models.BooleanField('exercise today?')
+    med_change = models.BooleanField('medication change?')
+    sleep_well = models.BooleanField('sleep well?')
+    notes = models.TextField('personal notes', null=True, blank=True)
+    date_logged = models.DateTimeField(auto_now=True)
+
+    @classmethod
+    def avg_mh_last(cls, num_logs):
+        recent_logs = cls.objects.order_by('-date_logged')[:num_logs]
+        
+        total = 0
+        for log in recent_logs:
+            total += log.mh_rating
+
+        return total / num_logs
+
+    def get_date_str(self):
+        return '{:%H:%M %m/%d/%Y}'.format(self.date_logged)
+
+    def __str__(self):
+        return f'{self.mh_rating}, {self.get_date_str()}'
