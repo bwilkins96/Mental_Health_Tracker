f707b4649dc3de37fbfc1a81e3012721c39f246a
bwilkins96 <ben.jr.speed@gmail.com>
2023-06-30 19:32:35 -0400

    javascript script to change color of index circles

---
+++ web_app_final/mhtracker/static/mhtracker/index_script.js
@@ -0,0 +1,31 @@
+function changeCircleColor(circle) {
+    const pEle = circle.querySelector('p');
+    const avg = Number(pEle.innerText);
+    let color = '';
+
+    if (!avg) {
+        return
+    } else if (avg < 2) {
+        color = 'indianred';
+    } else if (avg < 3) {
+        color = 'lightsalmon';
+    } else if (avg < 4) {
+        color = 'aquamarine';
+    } else {
+        color = 'springgreen';
+    }
+
+    circle.style.backgroundColor = color;
+}
+
+function main() {
+    const avgCircles = document.querySelectorAll('.circle');
+
+    for(let i = 0; i < avgCircles.length; i++) {
+        changeCircleColor(avgCircles[i]);
+    }
+
+    circle = avgCircles[0];
+}
+
+main()
