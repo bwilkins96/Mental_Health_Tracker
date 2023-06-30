c0671c6afcf84218b563cc165aba8ac0eed4b238
bwilkins96 <ben.jr.speed@gmail.com>
2023-06-30 18:35:46 -0400

    username unique validation and misc css

--- web_app_final/mhtracker/forms.py
+++ web_app_final/mhtracker/forms.py
@@ -1,5 +1,6 @@
 from django import forms   
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 class SignUpForm(forms.Form):
     username = forms.CharField()
@@ -11,3 +12,7 @@ class SignUpForm(forms.Form):
 
         if cleaned_data['password'] != cleaned_data['confirm_password']:
             raise ValidationError('Passwords do not match')
+        
+        if User.objects.filter(username=cleaned_data['username']).exists():
+            raise ValidationError('Username already exists')
+        
